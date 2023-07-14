1388fa3f69c8a10725a12f7916d28c7ba68441a7
FFIXL <al.mag31995@gmail.com>
2023-07-14 15:27:42 -0700

    2nd  0x01. C - Variables, if, else, while commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: the first digit is bigger than 5 or 0 or less than 6 and not 0
+ *
+ *Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	x = n % 10;
+	if (x > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n. x);
+	else if (x == 0)
+		printf("Last digit of %i is %i and is 0\n", n. x);
+	else
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n. x);
+	return (0);
+}
