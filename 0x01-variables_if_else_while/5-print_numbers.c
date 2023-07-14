524b10db2f4ae4288077b05d2f9250733674ce43
FFIXL <al.mag31995@gmail.com>
2023-07-14 16:13:42 -0700

    6th 0x01. C - Variables, if, else, while commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: all single digit numbers of base 10 starting from 0
+ *
+ *Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		printf("%i", x);
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
