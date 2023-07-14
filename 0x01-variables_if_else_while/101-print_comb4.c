6fa6fbf50c2ec6b91aef70049c8357a4edf38b03
FFIXL <al.mag31995@gmail.com>
2023-07-14 16:56:14 -0700

    12th 0x01. C - Variables, if, else, while commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: all different combinations of two digits no repeat
+ *
+ *Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	int x = 0;
+	int y = 0;
+	int z = 0;
+
+	while (x <= 9)
+	{
+		y = 0;
+		while (y <= 9)
+		{
+			z = 0;
+			while (z <= 9)
+			{
+				if (x < y && y < z)
+				{
+					putchar(x + 48);
+					putchar(y + 48);
+					putchar(z + 48);
+					if (x != 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				y++;
+			}
+			x++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
