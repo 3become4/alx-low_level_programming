016873fb21d6d28e71aae6b7008b731f408e4a30
FFIXL <al.mag31995@gmail.com>
2023-07-18 16:06:27 -0700

    11th 0x02. C - Functions, nested loops commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *add - Entry point
+ *
+ *Description: sum
+ *
+ * @x: input 1
+ * @y: input 2
+ *
+ *Return: sum of input
+*/
+
+int add(int x, int y)
+{
+	int sum = x + y;
+
+	return (sum);
+}
