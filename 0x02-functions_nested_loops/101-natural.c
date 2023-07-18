21e7ebbee841e68f79f230021d8159bfeaa4bebf
FFIXL <al.mag31995@gmail.com>
2023-07-18 16:35:44 -0700

    14th 0x02. C - Functions, nested loops commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *main - Entry point
+ *
+ *Description: prints the sum of all the multiples of 3 or 5 below 1024
+ *
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int s, i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			s += i;
+	}
+	printf("%d\n", s);
+}
