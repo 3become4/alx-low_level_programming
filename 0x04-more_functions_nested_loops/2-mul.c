0a5c07eefde048300db73933616726e00754f957
FFIXL <al.mag31995@gmail.com>
2023-07-20 06:54:44 -0700

    3rd 0x04. C - More functions, more nested loops commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *mul - Entry point
+ *
+ *Description: multiplies two integers.
+ *
+ * @a: an input of int
+ * @b: an input of int
+ *
+ * Return: x
+*/
+
+int mul(int a, int b)
+{
+	x = a * b;
+
+	return (x);
+}
