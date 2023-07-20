60f197ccebce25beb9ff619e89e9e632229bfc6b
FFIXL <al.mag31995@gmail.com>
2023-07-20 09:30:37 -0700

    10th 0x04. C - More functions, more nested loops commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *main - Entry point
+ *
+ *Description: 1 to 100, with conditions.
+ *
+ *
+ * Return: 0
+*/
+
+void main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
