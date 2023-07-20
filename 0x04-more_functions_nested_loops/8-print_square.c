573c7f4c2547f494744e4889bd2c2957998f049c
FFIXL <al.mag31995@gmail.com>
2023-07-20 08:46:05 -0700

    9th 0x04. C - More functions, more nested loops commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_square - Entry point
+ *
+ *Description: prints a square
+ *
+ * @size: number
+ *
+ * Return: result
+*/
+
+void print_square(int size)
+{
+	int i, l;
+
+	for (i = 0; i < size; i++)
+	{
+		if (n <= 0)
+			break;
+		for (l = 0; l < size; l++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
