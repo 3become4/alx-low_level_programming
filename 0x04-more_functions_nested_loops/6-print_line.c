8f287939b5159a1809e1f2c251dd887b441b9f72
FFIXL <al.mag31995@gmail.com>
2023-07-20 08:06:38 -0700

    7th 0x04. C - More functions, more nested loops commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_line - Entry point
+ *
+ *Description: draws a straight line in the terminal.
+ *
+ * @n: number
+ *
+ * Return: result
+*/
+
+void print_line(int n)
+{
+	for (i = 0; i < n; i++)
+	{
+		if (n <= 0)
+			break;
+		_putchar('_');
+	}
+	_putchar('\n');
+}
+
