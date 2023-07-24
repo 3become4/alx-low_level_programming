86498a4ec29dac32a697deaad74492ee62bcba0e
FFIXL <al.mag31995@gmail.com>
2023-07-24 10:46:40 -0700

    10th 0x05. C - Pointers, arrays and strings commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strcpy -  prints n elements of an array
+*
+* @dest: char pointer 1
+* @src: char  pointer 2
+*
+* Return: dest
+*/
+
+char *_strcpy(char *dest, char *src);
+{
+	int i = 0;
+
+	while (scr[i])
+	{
+		dest[i] = scr[i];
+		i++
+	}
+	return (dest);
+}
