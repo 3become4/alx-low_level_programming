424506eb48e2ce101cafdfd5fcd04cf32933c547
FFIXL <al.mag31995@gmail.com>
2023-07-24 12:11:32 -0700

    12th 0x05. C - Pointers, arrays and strings commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,33 +3,33 @@
 #include <time.h>
 
 /**
-* main - generates random valid passwords
-*
-*
-* Return: 0
-*/
-
+ * main - generates random password
+ *
+ * Return: 0
+ */
 int main(void)
 {
-	int password[100];
+	int pass[80];
 	int i, x, y;
 
-	x = 0;
+	x = 0;	
 
 	srand(time(NULL));
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i < 80; i++)
 	{
-		password[i] = rand() % 78;
-		x += (password[i] + '0');
-		putchar(password[i] + '0');
+		pass[i] = rand() % 78;
+		x += (pass[i] + '0');
+		putchar(pass[i] + '0');
 		if ((2772 - x) - '0' < 78)
 		{
 			y = 2772 - x - '0';
 			x += y;
-			putchar(x + '0');
+			putchar(y + '0');
 			break;
 		}
 	}
+
 	return (0);
 }
+
