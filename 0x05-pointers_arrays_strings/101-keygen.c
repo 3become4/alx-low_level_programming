b7600d59698ccc35ad7dbf716f095ca9e30e5079
FFIXL <al.mag31995@gmail.com>
2023-07-24 12:04:18 -0700

    12th 0x05. C - Pointers, arrays and strings commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -21,13 +21,13 @@ int main(void)
 	for (i = 0; i < 100; i++)
 	{
 		password[i] = rand() % 78;
-		x += (password[i] + 48);
-		putchar(password[i] + 48);
-		if ((2772 - x) - 48 < 78)
+		x += (password[i] + '0');
+		putchar(password[i] + '0');
+		if ((2772 - x) - '0' < 78)
 		{
-			y = 2772 - x - 48;
+			y = 2772 - x - '0';
 			x += y;
-			putchar(x + 48);
+			putchar(x + '0');
 			break;
 		}
 	}
