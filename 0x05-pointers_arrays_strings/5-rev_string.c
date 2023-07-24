96a841a310a6d95901154fc38f7b1611920ae2e9
FFIXL <al.mag31995@gmail.com>
2023-07-24 09:50:42 -0700

    6th 0x05. C - Pointers, arrays and strings commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* rev_string - reverses a string.
+*
+* @s: string pointer
+*
+* Return: void
+*/
+
+void rev_string(char *s)
+{
+	int i = 0;
+	char x;
+
+	while (s[i] && s[i] != '\0')
+		i++;
+	for (int l = 0; l < i; l--)
+	{
+		x = s[l];
+		s[l] = s[i - l];
+		s[i - 1 - l] = x;
+	}
+
+}
