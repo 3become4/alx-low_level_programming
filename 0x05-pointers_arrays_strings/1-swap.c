ad1a59e144876adfd184aadd8a06bbc33982567d
FFIXL <al.mag31995@gmail.com>
2023-07-24 08:41:51 -0700

    2nd 0x05. C - Pointers, arrays and strings commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* swap_int - swaps the values of two integers.
+*
+* @a: int 1 pointer
+* @b: int 2pointer
+*
+* Return: void
+*/
+
+void swap_int(int *a, int *b)
+{
+	x = *a;
+	*a = *b;
+	*b = x;
+}
+
