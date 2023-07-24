7c327acd66fa7516ea4777411e41cddc0f2740f2
FFIXL <al.mag31995@gmail.com>
2023-07-24 09:09:53 -0700

    5th 0x05. C - Pointers, arrays and strings committh 0x05. C - Pointers, arrays and strings commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_rev - prints a string, in reverse
+*
+* @s: string 1 pointer
+*
+* Return: void
+*/
+
+void print_rev(char *s);
+{
+	i = _strlen(s);
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
