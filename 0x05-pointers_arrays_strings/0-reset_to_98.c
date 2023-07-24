545fad835c8b58f7070a677aca4df0a9ef21e874
FFIXL <al.mag31995@gmail.com>
2023-07-24 08:37:45 -0700

    1st 0x05. C - Pointers, arrays and strings commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,15 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
 * reset_to_98 - sets pointer to 98
-* 
+*
 * @n: month in number format
-* 
+*
 * Return: void
 */
 
 void reset_to_98(int *n)
 {
-	n = 98
+	*n = 98
 }
 
