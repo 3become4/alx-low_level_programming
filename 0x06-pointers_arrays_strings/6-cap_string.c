92b8cace2a3c420fa26efc6958fca443ed4b155d
FFIXL <al.mag31995@gmail.com>
2023-07-27 13:08:25 -0700

    7th 0x06. C - More pointers, arrays and strings commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_string - all uppercase.
+ *
+ * @s: string input
+ *
+ * Return: string
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+	int l = 0;
+	int dd = 0;
+	char d[] = " \t\n,.!?\"(){}";
+
+	while (s[i] && s[i] != '\0')
+	{
+		for (l = 0; l < 12; l++)
+		{
+			if (s[i] == d[l])
+				dd = 1;
+		}
+		if (s[i] >= 97 && s[i] <= 122 && dd == 1)
+		{
+			s[i] = s[i] - 32;
+			dd = 0;
+		}
+		else
+			dd = 0;
+		i++;
+	}
+	return (s);
+}
