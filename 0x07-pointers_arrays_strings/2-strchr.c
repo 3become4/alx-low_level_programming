bc93c952342715ee59acccbed0ea26f412404377
FFIXL <al.mag31995@gmail.com>
2023-07-31 13:24:42 -0700

    3rd 0x07. C - Even more pointers, arrays and strings 2nd try commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,10 +12,10 @@ char *_strchr(char *s, char c)
 	int i;
 
 	i = 0;
-	while (s[i] != '\0')
+	while (s[i] >= '\0')
 	{
 		if (s[i] == c)
-			return (s + (i - 1));
+			return (s + i);
 		i++;
 	}
 	return (0);
