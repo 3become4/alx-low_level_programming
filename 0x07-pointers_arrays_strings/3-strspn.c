67ab2a7f106d214b18104a6235b9fdbf121af082
FFIXL <al.mag31995@gmail.com>
2023-07-31 02:02:33 -0700

    4th 0x07. C - Even more pointers, arrays and strings commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strspn - length of a prefix substring
+ *
+ * @s: char input pointer
+ * @accept: char pointer
+ *
+ * Return: number of bytes in the initial segment.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+
+	i = j = 0;
+	while (s[i] && s[i] != '\0')
+	{
+		while (accept[j] != s[i])
+		{
+			if (accept[j] == '\0')
+				return (i);
+			j++
+		}
+
+		i++
+	}
+	return (i);
+}
