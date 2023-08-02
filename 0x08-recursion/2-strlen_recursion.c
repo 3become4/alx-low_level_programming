42c75ea8704691d5a4c0cc4150c3e393b7ff6db6
FFIXL <al.mag31995@gmail.com>
2023-08-02 13:18:14 -0700

    3rd  0x08. C - Recursion comit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen_recursion -   length of a string.
+ *
+ * @s: char pointer
+ *
+ *
+ * Return: len of string
+ */
+int _strlen_recursion(char *s);
+{
+	int i = 0;
+
+	if (*s > '\0')
+	{
+		i += _strlen_recursion(s + 1) + 1;
+	}
+	return (i);
+}
