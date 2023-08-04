ec92c552fb15731b4efd381cb760fe9fc22ce8a0
FFIXL <al.mag31995@gmail.com>
2023-08-04 12:35:53 -0700

    5th 0x0A. C - argc, argv commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,22 +7,21 @@
  * @argv: const arg
  *
  *
- * Return: 0 
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
 	int sum = 0;
-	int i;
 	char *x;
 
-	for (i = argc; i > 0; i--)
+	while (--argc)
 	{
-		for (x = argv[i]; *x; x++)
+		for (x = argv[argc]; *x; x++)
 		{
 			if (*x < '0' || *x > '9')
 				return (printf("Error\n"), 1);
 		}
-		sum += atoi(argv[i]);
+		sum += atoi(argv[argc]);
 	}
 	printf("%d\n", sum);
 	return (0);
