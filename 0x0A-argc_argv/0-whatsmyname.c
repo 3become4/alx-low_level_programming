3173292d0dc83d1f5584cd282acb87d765f451cb
FFIXL <al.mag31995@gmail.com>
2023-08-04 11:58:21 -0700

    1st 0x0A. C - argc, argv commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -  return the prog name
+ *
+ * @argc: int
+ * @argv: const arg
+ *
+ *
+ * Return: void
+ */
+int int main(int argc, char const *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
