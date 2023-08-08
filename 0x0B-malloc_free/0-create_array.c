a2249a3563acf28d38158ae15edfeae5b5a04277
FFIXL <al.mag31995@gmail.com>
2023-08-08 12:51:33 -0700

    1st 0x0B. C - malloc, free commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * create_array - creates an array initializes with specific char
+ *
+ * @size: int
+ * @c: char pointer st
+ *
+ * Return: 1 if the strings are identical
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *x = malloc(size);
+
+	if (size == 0 || n == 0)
+		return (0);
+	while (size--)
+		n[size] = c;
+	return (0);
+}
