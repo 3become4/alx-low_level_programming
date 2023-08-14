719794a2f9dea75182caa282279a43d36835b282
FFIXL <al.mag31995@gmail.com>
2023-08-14 15:42:11 -0700

    1st  0x0E. C - Structures, typedef commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,20 @@
 #ifndef dog_h
 #define dog_h
+/**
+ * dog - dog data structure
+ *
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * discription: a dog struct file.
+ */
 
 struct dog
 {
-    char *name;
-    float *age;
-    char *owner;
+	char *name;
+	float *age;
+	char *owner;
 }
 
 #endif
