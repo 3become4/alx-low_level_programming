e6debbdecb501aa3d8317d1c6700e157de475ba4
FFIXL <al.mag31995@gmail.com>
2023-08-14 16:06:21 -0700

    3rd  0x0E. C - Structures, typedef commit

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - function that initialize a variable of type struct dog
+ *
+ * @d: init dog
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
