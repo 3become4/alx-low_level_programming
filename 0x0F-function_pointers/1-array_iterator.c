df7f7493fcc3db446a30776af8f5324d98237cad
FFIXL <al.mag31995@gmail.com>
2023-08-16 17:51:33 -0700

    2nd 0x0F. C - Function pointers commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name.
+ *
+ * @array: int array
+ * @size: size
+ * @action: function pointer
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int *x = array + size - 1;
+
+	if (array && size && action)
+	{
+		while (array <= x)
+			action(*array++);
+	}
+}
