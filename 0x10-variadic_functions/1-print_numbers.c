3e6cfae9f7a5c51605d9fcc5a8aba0445a857cab
FFIXL <al.mag31995@gmail.com>
2023-08-17 09:54:59 -0700

    2nd  0x10. C - Variadic functions commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+* print_numbers -  prints numbers, followed by a new line.
+*
+* @separatorn: seperator.
+* @n: count of nums
+* @...: perms to print
+*
+* Return: void
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list x;
+	unsigned int i;
+
+	va_start(x, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(x, int));
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(x);
+}
