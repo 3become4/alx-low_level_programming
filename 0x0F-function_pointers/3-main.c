4c3b7e8969053cbcf0e38b676ab36f7ad88ffc38
FFIXL <al.mag31995@gmail.com>
2023-08-16 18:23:47 -0700

    4th  0x0F. C - Function pointers commit

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include "3-calc.h"
+
+/**
+ * main - Prints the result operations.
+ *
+ * @argc: num of arg.
+ * @argv: pointer array
+ *
+ * Return: 0
+ */
+ int main(int argc, char **argv)
+ {
+ 	int x, y;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	x = atoi(argv[1]);
+	op = argv[2];
+	y = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op == '/' && y == 0) || (*op == '%' && y == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(x, y));
+	return (0);
+ }
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,72 @@
+#include "3-calc.h"
+
+/**
+ * op_add -  add 2 integers.
+ *
+ * @a: int number
+ * @b: int number
+ *
+ * Return: int
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub -  subtract 2 integers.
+ *
+ * @a: int number
+ * @b: int number
+ *
+ * Return: int
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul -  multiply 2 integers.
+ *
+ * @a: int number
+ * @b: int number
+ *
+ * Return: int
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div -  devide 2 integers.
+ *
+ * @a: int number
+ * @b: int number
+ *
+ * Return: int
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod -  devide 2 integers.
+ *
+ * @a: int number
+ * @b: int number
+ *
+ * Return: int
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
