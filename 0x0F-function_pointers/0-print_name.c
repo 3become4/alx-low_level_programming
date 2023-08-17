e4e366c6b2df2deed172ac6421cb16c0ccefdfd5
FFIXL <al.mag31995@gmail.com>
2023-08-16 17:35:36 -0700

    1st 0x0F. C - Function pointers commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_name - function that prints a name.
+ *
+ * @name: name
+ * @f: printer function
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
