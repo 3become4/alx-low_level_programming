a1013a63b8ea7ff6bfd0483fff839c5166d4fc92
Ali maged <al.mag31995@gmail.com>
2023-08-24 16:29:49 -0700

    5th 0x12. C - Singly linked lists commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_list - that frees a list_t list.
+ *
+ * @head: pointer
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *x, *y;
+
+	x = head;
+	while (x)
+	{
+		y = x->next;
+		free(x->str);
+		free(x);
+		x= next_node;
+	}
+}
+
