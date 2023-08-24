f087130a9f437f495e8664d243ee721664838d85
Ali maged <al.mag31995@gmail.com>
2023-08-24 16:12:53 -0700

    4th 0x12. C - Singly linked lists commit

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - dds a new node at the end of a list_t list.
+ *
+ * @head: double pointer
+ * @str: new string
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *x;
+	list_t *temp = *head;
+	unsigned int l = 0;
+
+	while (str[l])
+		l++;
+
+	x = malloc(sizeof(list_t));
+	if (!x)
+		return (NULL);
+
+	x->str = strdup(str);
+	x->len = l;
+	x->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = x;
+		return (x);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = x;
+
+	return (x);
+}
+
