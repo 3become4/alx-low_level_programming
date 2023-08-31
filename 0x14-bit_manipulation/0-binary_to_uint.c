f8d2518d4086c4c8d4c6435aaeb9febde81d2fdb
Ali maged <al.mag31995@gmail.com>
2023-08-31 16:49:09 -0700

    1st  0x14. C - Bit manipulation commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,19 +9,17 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
 	unsigned int x = 0;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	while (*b)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (*b != '0' || *b != '1')
 			return (0);
-		x = 2 * x + (b[i] - '0');
+		x = 2 * x + (*b++ - '0');
 	}
-
 	return (x);
 }
 
