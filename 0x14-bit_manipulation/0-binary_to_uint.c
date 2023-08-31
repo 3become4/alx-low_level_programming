dfad8bd74f4d6f5608e15637b4a1591bf7b7c7a0
Ali maged <al.mag31995@gmail.com>
2023-08-31 16:47:09 -0700

    1st  0x14. C - Bit manipulation commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * binary_to_uint-  converts a binary number to an unsigned int.
+ *
+ * @b: char pointer
+ *
+ * Return: the converted number, or 0 
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int x = 0;
+
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		x = 2 * x + (b[i] - '0');
+	}
+
+	return (x);
+}
+
