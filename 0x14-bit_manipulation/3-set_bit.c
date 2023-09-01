00a46c3eecb6df7686058bf23d5331bd775f689a
Ali maged <al.mag31995@gmail.com>
2023-08-31 17:49:56 -0700

    4th 0x14. C - Bit manipulation commit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_bit- returns the value of a bit at a given index.
+ *
+ * @n: int parameter
+ * @index: index for the bit
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int x;
+
+	if (index > 63)
+		return (-1);
+
+	x = 1 << index;
+	*n = (*n | x);
+
+	return (1);
+}
